f7e4fe272c6743ce65352c345203783bfa93f583
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2018-04-12 15:06:11 -0500

    Initial Commit

---
+++ MoveAlongDroid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoveAlongDroid
+//
+//  Created by Nathan on 4/12/18.
+//  Copyright © 2018 Nathan Fulkerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
